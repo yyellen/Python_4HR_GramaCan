a686bed16399fb5b08cd919877d7d53f4ea9f139
yyellen <yyopenfolder@gmail.com>
2023-05-19 14:23:05 +0800

    add: 10.dictionary.py

---
+++ 10.dictionary.py
@@ -0,0 +1,13 @@
+# 字典 dictionary
+# key value
+# 鍵  值
+
+dic = {"蘋果":"apple", "香蕉":"banana", "貓":"cat", "狗":"dog"}
+print(dic)
+print(dic["蘋果"])
+print(dic["貓"])
+
+
+dic2 = {0:"apple", 1:"banana", 2:"cat", 3:"dog"}
+print(dic2[0])
+print(dic2[3])
