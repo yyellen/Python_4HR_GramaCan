749ddedf8a598e82edfc4d16a82f11e5de3b42ee
yyellen <yyopenfolder@gmail.com>
2023-05-22 16:53:05 +0800

    add: 16.module.py

---
+++ 16.module.py
@@ -0,0 +1,14 @@
+# 模組 module 的使用
+# Python Module List: https://docs.python.org/3/py-modindex.html# 
+# pip 套件管理工具
+
+import tool
+import token
+import sys
+import numpy as np
+
+# print(tool.name)
+# print(tool.age)
+# print(tool.max_num(2, 3, 88))
+
+# print(sys.path)
