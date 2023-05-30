3fbc933c967987b1aa8f59a97462ae3e2766be21
yyellen <yyopenfolder@gmail.com>
2023-05-30 11:48:17 +0800

    add: inheritance

---
+++ student.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, name, age, school):
+        self.name = name
+        self.age = age
+        self.school = school
+    
+    def print_name(self):
+        print(self.name)
+    
+    def print_age(self):
+        print(self.age)
+
+    def print_school(self):
+        print(self.school)
