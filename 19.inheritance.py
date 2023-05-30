2886643f314a92318e0169326169399ce7b8b14d
yyellen <yyopenfolder@gmail.com>
2023-05-30 12:08:58 +0800

    add: 19.inheritance.py

---
+++ 19.inheritance.py
@@ -0,0 +1,7 @@
+# 繼承
+from student import Student
+
+student1 = Student("小白", 87, "小白國小")
+student1.print_school() # 小白國小
+student1.print_name() # 小白
+student1.print_age() # 87
--- student.py
+++ student.py
@@ -1,14 +1,12 @@
-class Student:
+from person import Person
+
+class Student(Person):
     def __init__(self, name, age, school):
         self.name = name
         self.age = age
         self.school = school
-    
-    def print_name(self):
-        print(self.name)
-    
-    def print_age(self):
-        print(self.age)
+
+    # 刪除重複的函式
 
     def print_school(self):
         print(self.school)
