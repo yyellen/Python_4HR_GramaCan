7d88dcabddd51b89b464a2e4027b8b1831474b33
yyellen <yyopenfolder@gmail.com>
2023-05-30 15:46:51 +0800

    update: inheritance super() function

--- student.py
+++ student.py
@@ -2,8 +2,8 @@ from person import Person
 
 class Student(Person):
     def __init__(self, name, age, school):
-        self.name = name
-        self.age = age
+        # super()函式將使子類別繼承其父類別的所有方法和屬性：https://www.w3schools.com/python/python_inheritance.asp
+        super().__init__(name, age)
         self.school = school
 
     # 刪除重複的函式
